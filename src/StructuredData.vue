01676affc31e34181250694781721f88e3f64f3a
Andrew Zigler <andrewzigler@gmail.com>
2019-06-16 15:12:57 -0700

    Package component for npm
    - v1.1.3 published to npm, first working version

---
+++ src/StructuredData.vue
@@ -0,0 +1,37 @@
+<template>
+  <script type="application/ld+json" v-html="schemaData" />
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: ''
+    },
+    properties: {
+      type: Object,
+      default() {
+        return {}
+      }
+    },
+    schema: {
+      type: Object,
+      default() {
+        return {}
+      }
+    }
+  },
+  data() {
+    const schemaKey = `${this.type.charAt(0).toLowerCase() +
+      this.type.slice(1)}Schema`
+    const schemaData = JSON.stringify({
+      ...this.schema[schemaKey],
+      ...this.properties
+    })
+    return {
+      schemaData
+    }
+  }
+}
+</script>
